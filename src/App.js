08248e624049731b2600f22614a081c6a2c6f41d
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-17 21:13:23 -0300

    Made Basic Desgin

--- src/App.js
+++ src/App.js
@@ -2,24 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Navbar extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render () {
+    return(
+      <nav class="navbar">  
+        <a id="project-name">[PROJECT_NAME]</a>
+        <a class="menu">
+          <div></div>
+          <div></div>
+          <div></div>
+        </a>
+      </nav>
+    )
+  }
+}
+
+class CodeField extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="code-container">
+        <textarea cols="40" rows="5" id="code"></textarea>
+      </div>
+    )
+  }
+}
+
+class Console extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+        <textarea cols="40" rows = "5" id="console" disabled="true">Hello</textarea>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar />
+        <CodeField />
+        <Console />
       </div>
     );
   }
